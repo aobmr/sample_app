0513d17a92b9b1e03ff5eaf191b62b8880bdea53
aobmr <a20z1ma@aiit.ac.jp>
2022-05-29 17:33:25 +0000

    Add user following

--- db/seeds.rb
+++ db/seeds.rb
@@ -20,9 +20,18 @@ User.create!(name:  "Example User",
                password:              password,
                password_confirmation: password)
 end
-               
+
+#マイクロポスト               
 users = User.order(:created_at).take(6)
 50.times do
   content = Faker::Lorem.sentence(word_count: 5)
   users.each { |user| user.microposts.create!(content: content) }
 end
+
+#以下のリレーションシップを作成
+users = User.all
+user = users.first
+following = users[2..50]
+followers = users[3..40]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
